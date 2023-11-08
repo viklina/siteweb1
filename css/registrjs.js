3dead4d9743fb9c589aabaa59c192ae7bdc7a190
vikilina <vikapletneva32@gmail.com>
2023-11-08 16:07:25 +0300

    08.11

--- css/registrjs.js
+++ css/registrjs.js
@@ -1,46 +1,67 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("submit").addEventListener("click", function (event) {
-        event.preventDefault(); // Prevent the default form submission if inside a form
+    document.getElementById("knopka1").addEventListener("click", function (event) {
+        event.preventDefault();
         var name = document.getElementById("name").value;
         var mail = document.getElementById("mail").value;
         var password = document.getElementById("password").value;
         var password2 = document.getElementById("password2").value;
 
-        if (name === "" && mail === "" && password === "" && password2 === "") {
+        if (name === "" || mail === "" || password === "" || password2 === "") {
             var russianWord = "Заполните все поля";
             console.log(russianWord);
-        } else if (name === "") {
-            var russianWord1 = "Заполните поле Имя";
-            console.log(russianWord1);
-        } else if (mail === "") {
-            var russianWord2 = "Заполните поле Почта";
-            console.log(russianWord2);
-        } else if (password === "") {
-            var russianWord3 = "Заполните поле Пароль";
-            console.log(russianWord3);
-        } else if (password2 === "") {
-            var russianWord4 = "Заполните поле Повторите пароль";
-            console.log(russianWord4);
         } else if (password !== password2) {
             var russianWord5 = "Пароли не совпадают";
             console.log(russianWord5);
         } else {
             var userInfo = "Имя: " + name + ", Почта: " + mail + ", Пароль: " + password + " || Вы зарегистрированы!";
             console.log(userInfo);
-        
+
             fetch('http://localhost:3000/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ name, mail, password, password2 })
+                body: JSON.stringify({ name, mail, password })
             })
                 .then(response => response.json())
                 .then(data => {
-                    console.log(data.message); // Вывод ответа от сервера
+                    console.log(data); // Вывод ответа от сервера
+                    if (data.message === 'Пользователь зарегистрирован') {
+                        // Здесь можете выполнить дополнительные действия после регистрации
+                        console.log("Пользователь зарегистрирован успешно!");
+                    }
                 })
-                .catch(error => console.error('Ошибка:', error)); }
-        });
-});
+                .catch(error => console.error('Ошибка:', error));
+        }
+    });
 
-  
+    document.getElementById("knopka2").addEventListener("click", function (event) {
+        event.preventDefault();
+
+        var mail = document.getElementById("mail1").value;
+        var password = document.getElementById("parol").value;
+
+        if (mail === "" || password === "") {
+            console.log("Заполните все поля для входа");
+        } else {
+            fetch('http://localhost:3000/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ mail, password })
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data); // Вывод ответа от сервера
+                    if (data.token) {
+                        localStorage.setItem('token', data.token); // Сохранение токена в localStorage
+                        console.log("Успешный вход!");
+                    } else {
+                        console.log("Ошибка входа, проверьте правильность введенных данных");
+                    }
+                })
+                .catch(error => console.error('Ошибка:', error));
+        }
+    });
+});
---
+++ server.js
@@ -0,0 +1,82 @@
+
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const app = express();
+const jwt = require('jsonwebtoken');
+
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use('/css', express.static(site + '/css'));
+app.use('/images', express.static(site + '/images'));
+
+app.get('/', (req, res) => {
+    res.sendFile('site' + '/index.html');
+});
+
+//app.use((req, res, next) => {
+    //res.setHeader('Content-Security-Policy', "default-src 'self' img-src 'self'");
+    //next();
+//});
+
+app.post('/register', (req, res) => {
+    const { name, mail, password } = req.body;
+    const newUser = { name, mail, password };
+
+    if (!fs.existsSync('users.json')) {
+        fs.writeFileSync('users.json', '[]'); // Создание пустого массива, если файл не существует
+    }
+
+    let users = JSON.parse(fs.readFileSync('users.json'));
+
+    // Добавление нового пользователя
+    users.push(newUser);
+
+    // Запись обновленных данных в файл users.json
+    fs.writeFileSync('users.json', JSON.stringify(users, null, 2));
+
+    res.json({ message: 'Пользователь зарегистрирован' });
+});
+
+app.post('/login', (req, res) => {
+    const { mail, password } = req.body;
+
+    let users = JSON.parse(fs.readFileSync('users.json'));
+
+    const user = users.find((user) => user.mail === mail && user.password === password);
+
+    if (user) {
+        const token = jwt.sign({ user }, 'your_secret_key'); // Создание токена
+
+        res.json({ token }); // Отправка токена клиенту
+    } else {
+        res.status(401).json({ message: 'Ошибка аутентификации' });
+    }
+});
+// Пример middleware для проверки токена при запросах, требующих аутентификации
+function verifyToken(req, res, next) {
+    const token = req.headers['authorization'];
+
+    if (!token) {
+        return res.status(403).json({ message: 'Отсутствует токен' });
+    }
+
+    jwt.verify(token, 'your_secret_key', (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: 'Недействительный токен' });
+        }
+        req.user = decoded.user;
+        next();
+    });
+}
+
+// Пример защищенного маршрута, требующего аутентификации
+app.get('/protected', verifyToken, (req, res) => {
+    res.json(req.user);
+});
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
+});
